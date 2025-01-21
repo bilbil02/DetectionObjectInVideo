de3ea0bcf63207744bd0ae8023415ea8a05ba605
bilbil02 <nabilazahra27021997@gmail.com>
2025-01-21 10:37:04 +0700

    Update dl.py

--- dl.py
+++ dl.py
@@ -10,15 +10,15 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
-# URL file yolov3.weights yang diunggah ke penyimpanan cloud
-url = 'https://drive.google.com/file/d/1Ifv2TgD0KpVYG7cKHB0GVPcJF4XhCWgR/view?usp=sharing'  # Ganti dengan URL file Anda
+# # URL file yolov3.weights yang diunggah ke penyimpanan cloud
+# url = 'https://drive.google.com/file/d/1Ifv2TgD0KpVYG7cKHB0GVPcJF4XhCWgR/view?usp=sharing'  # Ganti dengan URL file Anda
 
-# Cek dan unduh file yolov3.weights
-if not os.path.exists('yolov3.weights'):
-    st.write("Mengunduh file yolov3.weights...")
-    r = requests.get(url)
-    with open('yolov3.weights', 'wb') as f:
-        f.write(r.content)
+# # Cek dan unduh file yolov3.weights
+# if not os.path.exists('yolov3.weights'):
+#     st.write("Mengunduh file yolov3.weights...")
+#     r = requests.get(url)
+#     with open('yolov3.weights', 'wb') as f:
+#         f.write(r.content)
     
 # Load YOLO model
 def load_yolo_model():
